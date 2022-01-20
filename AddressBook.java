233eb323cdbc91233b502b5bafe32407ca9bd1f8
suhantsakunde07 <sushantsakunde07@gmail.com>
2022-01-20 05:49:21 +0530

    Added: Add New Contact

--- AddressBook.java
+++ AddressBook.java
@@ -1,33 +1,40 @@
 package AddressBook;
 
+import java.util.ArrayList;
+
 import java.util.*;
 
 public class AddressBook {
+    ArrayList<Person> list = new ArrayList<Person>();
+    Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
+
         System.out.println("Welcome to address book ");
         AddressBook addressBook = new AddressBook();
-        System.out.println("Enter details to create contact in address book: ");
+        System.out.println("Enter details to create contact in address book:");
         addressBook.createContact();
     }
 
     public void createContact() {
-
-        Scanner scanner = new Scanner(System.in);
+        Person details = new Person();
         System.out.println("Enter your first name: ");
-        String firstName = scanner.nextLine();
+        details.setFirstName(scanner.nextLine());
         System.out.println("Enter your last name: ");
-        String lastName = scanner.nextLine();
+        details.setLastName(scanner.next());
         System.out.println("Enter your address : ");
-        String address = scanner.nextLine();
+        details.setAddress(scanner.next());
         System.out.println("Enter your city name: ");
-        String city = scanner.nextLine();
+        details.setCity(scanner.next());
         System.out.println("Enter your state name: ");
-        String state = scanner.nextLine();
+        details.setState(scanner.next());
         System.out.println("Enter your zip code: ");
-        int zipcode = scanner.nextInt();
+        details.setZip(scanner.nextInt());
         System.out.println("Enter your phone number: ");
-        long phoneNumber = scanner.nextLong();
+        details.setPhoneNumber(scanner.nextLong());
         System.out.println("Enter your email address : ");
-        String emailAddress = scanner.next();
+        details.setEmailAddress(scanner.next());
+        list.add(details);
+        System.out.println(list);
     }
 }

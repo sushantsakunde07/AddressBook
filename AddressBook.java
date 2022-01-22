a028064db9f15d7cac11e0559835a84664b83916
suhantsakunde07 <sushantsakunde07@gmail.com>
2022-01-22 07:47:57 +0530

    Added: Edit existing contact

--- AddressBook.java
+++ AddressBook.java
@@ -1,13 +1,18 @@
 package AddressBook;
 
 import java.util.*;
+import java.util.ArrayList;
 
 public class AddressBook {
+    ArrayList<Person> list = new ArrayList();
+    Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
         System.out.println("Welcome to address book ");
         AddressBook addressBook = new AddressBook();
-        System.out.println("Enter details to create contact in address book: ");
+        System.out.println("Enter details to create contact in address book:");
         addressBook.createContact();
+        addressBook.editContact();
     }
 
     public void createContact() {
@@ -29,5 +34,56 @@ public class AddressBook {
         long phoneNumber = scanner.nextLong();
         System.out.println("Enter your email address : ");
         String emailAddress = scanner.next();
+    }
+
+    public void editContact() {
+        System.out.println("Enter Name for Edit");
+        String editName = this.scanner.next();
+
+        for (int i = 0; i < this.list.size(); ++i) {
+            if (((Person) this.list.get(i)).getFirstName().equals(editName)) {
+                System.out.println("Select Options");
+                System.out.println("1: First Name\n 2:Last Name\n 3:Address\n 4:City Name\n 5:State Name\n 6: Zip Code\n 7:Phone Number\n 8:Email Address");
+                int option = this.scanner.nextInt();
+                switch (option) {
+                    case 1:
+                        System.out.println("Enter first name");
+                        ((Person) this.list.get(i)).setFirstName(this.scanner.next());
+                        break;
+                    case 2:
+                        System.out.println("Enter last name");
+                        ((Person) this.list.get(i)).setLastName(this.scanner.next());
+                        break;
+                    case 3:
+                        System.out.println("Enter address");
+                        ((Person) this.list.get(i)).setAddress(this.scanner.next());
+                        break;
+                    case 4:
+                        System.out.println("Enter city");
+                        ((Person) this.list.get(i)).setCity(this.scanner.next());
+                        break;
+                    case 5:
+                        System.out.println("Enter state");
+                        ((Person) this.list.get(i)).setState(this.scanner.next());
+                        break;
+                    case 6:
+                        System.out.println("Enter Zip Code");
+                        ((Person) this.list.get(i)).setZip(this.scanner.nextInt());
+                        break;
+                    case 7:
+                        System.out.println("Enter Phone Number");
+                        ((Person) this.list.get(i)).setPhoneNumber(this.scanner.nextLong());
+                        break;
+                    case 8:
+                        System.out.println("Enter Email Address");
+                        ((Person) this.list.get(i)).setEmailAddress(this.scanner.nextLine());
+                        break;
+                    default:
+                        System.out.println("Enter valid name");
+                }
+            }
+            System.out.println("Edited list is ");
+            System.out.println(this.list);
+        }
     }
 }
